4a551f79a22103fd4294446085723ac9a73238a7
andreaax <75586588+andreaax@users.noreply.github.com>
2022-06-30 18:21:59 -0400

    Estee Lauder

---
+++ esteeLauder/ProductsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductsViewController.swift
+//  esteeLauder
+//
+//  Created by Andrea Hurtado on 6/30/22.
+//
+
+import UIKit
+
+class ProductsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- esteeLauder/ViewController.swift
+++ esteeLauder/ViewController.swift
@@ -14,6 +14,42 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func YT(_ sender: UIButton) {
+        
+        if let url = URL(string: "https://www.youtube.com/c/EsteeLauderEL/featured") {
+                UIApplication.shared.open(url)
+              } else {
+                print("url is not correct")
+              }
+
+
+    }
+    
+    
+    @IBAction func INSTA(_ sender: Any) {
+    
+    
+        if let url = URL(string: "https://www.instagram.com/esteelauder/") {
+                UIApplication.shared.open(url)
+              } else {
+                print("url is not correct")
+              }
+        
+    }
+    
+    
+    @IBAction func TWITTER(_ sender: Any) {
+       
+        if let url = URL(string: "https://twitter.com/EsteeLauder") {
+                UIApplication.shared.open(url)
+              } else {
+                print("url is not correct")
+              }
+    
+    }
+    
+    
 
 }
 
